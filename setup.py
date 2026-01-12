83b00b8da9f56a175323a8c23dce559f395fd59e
Richard-Sti <richard.stiskalek@protonmail.com>
2026-01-12 15:37:36 +0100

    update dep

--- setup.py
+++ setup.py
@@ -4,5 +4,15 @@ setup(
     name="cdmprof",
     version="0.1.0",
     packages=find_packages(),
-    python_requires=">=3.8",
+    python_requires=">=3.11",
+    install_requires=[
+        "numpy",
+        "h5py",
+        "sympy",
+        "cffi",
+        "tqdm",
+    ],
+    extras_require={
+        "mpi": ["mpi4py"],
+    },
 )
