fb8bae963d491327b455001b33c6a8db18dcade5
Richard-Sti <richard.stiskalek@protonmail.com>
2026-01-22 18:56:43 +0000

    add NFW specific

--- cdmprof/__init__.py
+++ cdmprof/__init__.py
@@ -3,7 +3,7 @@
 from . import asymptotes, data, fitting, symbolic
 from .asymptotes import compute_asymptotes_with_params
 from .data import HaloData, load_from_folder
-from .fitting import compile_fitter
+from .fitting import compile_fitter, fit_nfw
 from .symbolic import SympyParser
 
 __version__ = "0.1.0"
@@ -16,6 +16,7 @@ __all__ = [
     "HaloData",
     "load_from_folder",
     "compile_fitter",
+    "fit_nfw",
     "compute_asymptotes_with_params",
     "SympyParser",
 ]
